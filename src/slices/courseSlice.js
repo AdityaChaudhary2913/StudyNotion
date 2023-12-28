e70dd55d7a600b94b466ce701d25493165f6f5f2
AdityaTheJaat <programmingID1306@gamil.com>
2023-12-28 15:48:23 +0530

    Refactor courseSlice.js to use shorthand syntax for initialState

--- src/slices/courseSlice.js
+++ src/slices/courseSlice.js
@@ -10,7 +10,7 @@ const initialState = {
 
 const courseSlice = createSlice({
   name: "course",
-  initialState: initialState,
+  initialState,
   reducers: {
     setStep: (state, action) => {
       state.step = action.payload
